352862d0c5e84fb7a4755de7a39ab272d2f043df
ThangTruong <thangtm6@fsoft.com.vn>
2017-03-10 09:01:07 +0700

    Add vehicle delegate and datasource

--- SplitDelegateDataSource/DataService.swift
+++ SplitDelegateDataSource/DataService.swift
@@ -28,10 +28,35 @@ class DataService {
         _students = [ "Thang", "Hien", "Hoang", "Duc"]
     }
     
-    func reorder(fromIndex: Int, toIndex: Int) {
+    func reorderStudent(fromIndex: Int, toIndex: Int) {
         guard fromIndex != toIndex else {return}
         swap(&_students[fromIndex], &_students[toIndex])
     }
+    
+    private var _vehicles: [String] = []
+    
+    var vehicles: [String] {
+        set {
+            _vehicles = newValue
+        }
+        get {
+            if _vehicles.count == 0 {
+                updatevehicles()
+            }
+            return _vehicles
+        }
+    }
+    
+    func updatevehicles() {
+        _vehicles = [ "Lexus", "Camry", "Mecxedec", "BMW", "Mitsubisi"]
+    }
+    
+    func reordervehicles(fromIndex: Int, toIndex: Int) {
+        guard fromIndex != toIndex else {return}
+        swap(&_vehicles[fromIndex], &_vehicles[toIndex])
+    }
+    
+    
     
 }
 
